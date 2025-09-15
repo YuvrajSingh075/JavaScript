c3d22e3fdef4c7ebb98d3b84d66f142b30b775a3
YuvrajSingh075 <singhyuvraj0374@gmail.com>
2025-09-15 07:02:55 +0000

    Variales in js done

---
+++ 01_basics/Variables.js
@@ -0,0 +1,12 @@
+const accountId = 231232;
+let accountEmail = "singhyuvraj0374@gamil.com";
+var accountPassoeword = "123456"
+accountCity = "Jaipur";
+
+// accountId = 2 not allowed
+
+accountEmail = "hc@hello.com"
+accountPassoeword = "121212"
+accountCity = "Noida"
+console.log(accountId); 
+console.table([accountId,accountEmail,accountPassoeword,accountCity])
