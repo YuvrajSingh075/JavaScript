085d49167901f38cc3db4f734be5c6cbe3f79d17
YuvrajSingh075 <singhyuvraj0374@gmail.com>
2025-09-15 08:42:30 +0000

    Conversion_opration

---
+++ 01_basics/conversion_operation.js
@@ -0,0 +1,22 @@
+let score = "Yuvraj"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+console.log(typeof NaN);
+
+// "33" - 33
+// "33abc" - NaN
+//true - 1; false - 0
+
+let islogggedIn = " "
+
+let booleanIslogged = Boolean(islogggedIn)
+console.log(booleanIslogged);
+
+let string = "23"
+let isStringNum = String(string);
+console.log(isStringNum);
