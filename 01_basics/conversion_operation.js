a3b9ea5dd5e3046ea72c465dc9c462255296836a
YuvrajSingh075 <singhyuvraj0374@gmail.com>
2025-09-19 12:08:43 +0000

    all done

--- 01_basics/conversion_operation.js
+++ 01_basics/conversion_operation.js
@@ -1,12 +1,12 @@
 let score = "Yuvraj"
 
-console.log(typeof score);
-console.log(typeof(score));
+// console.log(typeof score);
+// console.log(typeof(score));
 
 let valueInNumber = Number(score)
 console.log(typeof valueInNumber);
-console.log(valueInNumber);
-console.log(typeof NaN);
+// console.log(valueInNumber);
+// console.log(typeof NaN);
 
 // "33" - 33
 // "33abc" - NaN
@@ -20,3 +20,29 @@ console.log(booleanIslogged);
 let string = "23"
 let isStringNum = String(string);
 console.log(isStringNum);
+
+//* ************************ Opertions  ******************** 
+
+let value = 3
+let netvalue = -value
+// console.log(netvalue);
+
+// console.log(2%3)
+let str1 = "hello"
+let str2 = "Yuvraj"
+let str3 = str1+str2;
+console.log(str3);
+
+console.log("1" + 2)
+console.log(1 + "2")
+console.log("1" + 2 + 2) 
+console.log(1 + 2 + "2");
+
+console.log(+true);
+console.log(+"");
+
+
+
+let gameCounter = 100;
+gameCounter++;
+console.log(gameCounter)
