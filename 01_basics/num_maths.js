a4201598b26207cbdfabe72bf7bb32f9f1122ab8
YuvrajSingh075 <singhyuvraj0374@gmail.com>
2025-10-10 16:02:20 +0000

    Numbers done

---
+++ 01_basics/num_maths.js
@@ -0,0 +1,35 @@
+let score = 100;
+console.log(score);
+
+const balance = new Number(100);
+console.log(balance);
+
+console.log(balance.toString());
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));
+
+const otherNum = 123.89654
+console.log(otherNum.toPrecision(4));
+
+const hundreds = 10000000;
+console.log(hundreds.toLocaleString('en-IN'));
+
+//````````````````````````````Maths```````````````````````````````
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.5));
+// console.log(Math.ceil(4.4));
+// console.log(Math.floor(4.7));
+// console.log(Math.min(4,7,5,7.8));
+// console.log(Math.max(4,7.7,6,3));
+
+console.log(Math.floor(Math.random()*10)+1);
+
+const min = 10;
+const max = 20;
+
+console.log(Math.floor(Math.random() * (max - min + 1)+10));
+ 
+
+
